04cd39faf22686f78d1c5adf584a2ed6a90d0b06
Yathannsh <yathannsh@gmail.com>
2015-12-28 20:40:49 +0530

    Added Anchor2Anchor scroll

--- js/index.js
+++ js/index.js
@@ -218,3 +218,18 @@ $(document).ready(function() {
     images = $.makeArray($('.card-img-top[src=""]'));
   });
 });
+// Anchor to Anchor smooth scroll
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
