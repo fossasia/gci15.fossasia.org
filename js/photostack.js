6957c9c08404726af5250cf55d85ae884bcc9486
Abhi2424shek <abhi2424shekvashok@gmail.com>
2016-01-12 18:19:00 +0530

    Added Games Section

--- js/photostack.js
+++ js/photostack.js
@@ -1,4 +1,13 @@
-
+/**
+ * photostack.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2014, Codrops
+ * http://www.codrops.com
+ */
 ;( function( window ) {
 
 	'use strict';
